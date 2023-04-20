7b60550111f78df245f956e4e64e3bd3a37894d7
Brendan Duffy <brendan.p.duffy5@gmail.com>
2023-04-20 22:46:14 +0000

    completed lab 2

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,18 +1,39 @@
 import React from "react";
+import {Card, Button} from "react-bootstrap";
+
 
 class HornedBeast extends React.Component{
-    render(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            helloCounter: 0
+        }
+    }
 
-        // console.log(this.props)
+    incrementHello = ( ) => {
+        this.setState({
+            helloCounter: this.state.helloCounter + 1
+        })
+    }
 
+    render (){
         return(
-            <>
-                <p>{this.props.name}</p>
-                <p>{this.props.imageURL}</p>
+            <Card style={{width: '20rem'}}>
+                <Card.Img variant="top" src={this.props.image_url} alt={this.props.title} height={250} />
+                <Card.Body>
+                <Card.Title><h2>{this.props.title}</h2></Card.Title>
+                <Card.Text>
                 <p>{this.props.description}</p>
-            </>
+                <p>&#x2764;&#xFE0F;{this.state.helloCounter} times</p>
+                
+                </Card.Text>
+                <Button variant="primary" onClick={this.incrementHello} >Say Hello!</Button>
+
+                </Card.Body>
+            </Card>
         )
     }
 }
 
 export default HornedBeast;
+/* <img src="src/red heart.jpg" alt= "heart"/> */
